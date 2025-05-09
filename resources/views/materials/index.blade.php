e39e541f8669c2a047e4751bd209aaea130b9005
deknusapt <dev.deknusa@gmail.com>
2025-05-09 23:45:40 +0800

    feat: menambahkan view projects dan materials, serta controller untuk materials

---
+++ resources/views/materials/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1 class="text-2xl font-bold mb-4">All Materials</h1>
+    <p class="text-gray-500">Daftar semua project akan muncul di sini...</p>
+@endsection
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,6 @@
+@extends('dashboard.layout')
+
+@section('content')
+    <h1 class="text-2xl font-bold mb-4">All Projects</h1>
+    <p class="text-gray-500">Daftar semua projectttt akan muncul di sini...</p>
+@endsection
